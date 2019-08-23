8009a7addb190fa28b40b70c270bf66308b29286
7eezi <46866623+7eezi@users.noreply.github.com>
2019-08-23 20:05:42 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const token ="NjExODk4MjEwMzE0NzQ3OTE0.XWAIkQ.BZOah2c3g_WHy1zHgt4_btD0Av0";
+
+
+bot.on('ready', () =>{
+    console.log(`
+    
+    - This Bot is Online.
+    - Logged in ${bot.user.tag}.
+    - All Codes Working
+    
+    `)
+});
+
+
+bot.on('message', message => {
+    if (message.author.bot) return;
+    if (message.content.startsWith(".s")) {
+    if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('You dont have ADMINISTRATOR permission');
+    var args = message.content.trim().split(/ +/g).slice(1);
+    let cname = args[0];
+    let chan = message.guild.channels.find(element => element.name === cname);
+    if (chan) {
+    let text = args.slice(1).join(" ");
+    message.delete();
+    chan.send(text);
+    } else {
+    let text = args.join(" ");
+    message.delete();
+    message.channel.send(text);
+    }
+    }
+    });
+
+
+
+
+bot.login(token)
